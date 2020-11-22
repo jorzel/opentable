3ee5b837496b66219895bff38cc57e2711681f4b
Jarek Orzel <jaroslaw.orzel@emplocity.pl>
2020-11-22 15:41:34 +0100

    Table class with can_book functionality

---
+++ src/domain/table.py
@@ -0,0 +1,19 @@
+class TablePersonsException(Exception):
+    pass
+
+
+class Table:
+    def __init__(self, max_persons: int, is_open: bool = True):
+        if max_persons < 0:
+            raise TablePersonsException(
+                f"Table max_persons attribute must be positive integer, passed instead: {max_persons}"
+            )
+        self.max_persons = max_persons
+        self.is_open = is_open
+
+    def can_book(self, persons: int) -> bool:
+        if not self.is_open:
+            return False
+        if persons > self.max_persons:
+            return False
+        return True
