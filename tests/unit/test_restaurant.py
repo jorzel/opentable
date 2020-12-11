9f77f13fcc79f75afc0bdace8e63f6fba425bd0a
Jarek Orzel <jaroslaw.orzel@emplocity.pl>
2020-12-11 13:57:43 +0100

    factories for table/restaurant test objects

--- tests/unit/test_restaurant.py
+++ tests/unit/test_restaurant.py
@@ -1,32 +1,36 @@
-from src.domain.entities.table import Table
-from src.domain.entities.restaurant import Restaurant
-
-
-def test_restaurant_has_open_table_should_pass_if_any_table_in_restaurant_is_open_in_desired_capacity():
-    open_table = Table(max_persons=5, is_open=True)
-    restaurant = Restaurant(tables=[open_table])
+def test_restaurant_has_open_table_should_pass_if_any_table_in_restaurant_is_open_in_desired_capacity(
+    restaurant_factory, table_factory
+):
+    open_table = table_factory(max_persons=5, is_open=True)
+    restaurant = restaurant_factory(tables=[open_table])
 
     assert restaurant.has_open_table(3)
 
 
-def test_restaurant_has_open_table_should_failed_if_all_tables_are_occupied():
-    occupied_table = Table(max_persons=5, is_open=False)
-    restaurant = Restaurant(tables=[occupied_table])
+def test_restaurant_has_open_table_should_failed_if_all_tables_are_occupied(
+    restaurant_factory, table_factory
+):
+    occupied_table = table_factory(max_persons=5, is_open=False)
+    restaurant = restaurant_factory(tables=[occupied_table])
 
     assert not restaurant.has_open_table(3)
 
 
-def test_restaurant_has_open_table_should_failed_if_all_tables_with_desired_capacity_are_occupied():
-    occupied_table = Table(max_persons=5, is_open=False)
-    open_table = Table(max_persons=2, is_open=True)
-    restaurant = Restaurant(tables=[occupied_table, open_table])
+def test_restaurant_has_open_table_should_failed_if_all_tables_with_desired_capacity_are_occupied(
+    restaurant_factory, table_factory
+):
+    occupied_table = table_factory(max_persons=5, is_open=False)
+    open_table = table_factory(max_persons=2, is_open=True)
+    restaurant = restaurant_factory(tables=[occupied_table, open_table])
 
     assert not restaurant.has_open_table(3)
 
 
-def test_restaurant_book_table_should_pass_and_return_event_when_table_is_open_and_has_persons_capacity():
-    open_table = Table(max_persons=5, is_open=True)
-    restaurant = Restaurant(tables=[open_table])
+def test_restaurant_book_table_should_pass_and_return_event_when_table_is_open_and_has_persons_capacity(
+    restaurant_factory, table_factory
+):
+    open_table = table_factory(max_persons=5, is_open=True)
+    restaurant = restaurant_factory(tables=[open_table])
 
     table = restaurant.book_table(3)
 
--- tests/unit/test_table.py
+++ tests/unit/test_table.py
@@ -1,26 +1,29 @@
-from src.domain.entities.table import Table
-
-
-def test_can_book_table_should_pass_if_table_is_open_and_has_persons_capacity():
-    table = Table(is_open=True, max_persons=4)
+def test_can_book_table_should_pass_if_table_is_open_and_has_persons_capacity(
+    table_factory,
+):
+    table = table_factory(is_open=True, max_persons=4)
 
     assert table.can_book(3)
 
 
-def test_can_book_table_should_fail_if_table_is_not_open():
-    table = Table(is_open=False, max_persons=4)
+def test_can_book_table_should_fail_if_table_is_not_open(table_factory):
+    table = table_factory(is_open=False, max_persons=4)
 
     assert not table.can_book(3)
 
 
-def test_can_book_table_should_fail_if_table_is_open_but_exceed_persons_capacity():
-    table = Table(is_open=False, max_persons=4)
+def test_can_book_table_should_fail_if_table_is_open_but_exceed_persons_capacity(
+    table_factory,
+):
+    table = table_factory(is_open=False, max_persons=4)
 
     assert not table.can_book(6)
 
 
-def test_book_table_should_pass_and_return_event_when_table_is_open_and_has_persons_capacity():
-    table = Table(is_open=True, max_persons=4)
+def test_book_table_should_pass_and_return_event_when_table_is_open_and_has_persons_capacity(
+    table_factory,
+):
+    table = table_factory(is_open=True, max_persons=4)
 
     table.book(4)
 
